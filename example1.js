863e0b0df1af5f29ec5a474c037e0664831738cc
Steve Maraspin <s.maraspin@mvlabs.it>
2020-06-02 16:20:16 +0200

    minor rev to code

--- example1.js
+++ example1.js
@@ -1,9 +1,9 @@
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
+const axios = require('axios');
+
 const start = new Date()
 let end = 0;
-
-const axios = require('axios');
 
 axios.get('https://www.mocky.io/v2/5ed60dcf3400007b0006d626?mocky-delay=1000ms').then(resp => {
     console.log(resp.data);
