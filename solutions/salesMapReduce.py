03b4a9a03e08a7bede72dfce37e140beab1dc409
Steve Maraspin <s.maraspin@mvlabs.it>
2020-10-30 01:34:09 +0100

    MapReduce Python solution revised

--- solutions/salesMapReduce.py
+++ solutions/salesMapReduce.py
@@ -4,7 +4,7 @@ import json
 with open('../samples/sales.json', 'r') as f:
     sales_data = json.load(f)
 
-country_record = (list(map(lambda record: (str(record["year"]) + "-" + str(record["month"]), record["value"]), sales_data)))
+country_record = (list(map(lambda record: (record["value"]), sales_data)))
 #print(country_record)
 
 country_result = reduce(lambda val1, val2: val1 + val2, sorted(country_record))
