3783242fe98af435eb9d93d5d2596342b7a95375
Steve Maraspin <s.maraspin@mvlabs.it>
2020-11-02 23:39:17 +0100

    solution to sales calculation problem added

--- solutions/salesMapReduce.py
+++ solutions/salesMapReduce.py
@@ -5,7 +5,7 @@ with open('../samples/sales.json', 'r') as f:
     sales_data = json.load(f)
 
 country_record = (list(map(lambda record: (record["value"]), sales_data)))
-#print(country_record)
+print(country_record)
 
 country_result = reduce(lambda val1, val2: val1 + val2, sorted(country_record))
 print(country_result)
---
+++ solutions/salesMapReduceSpark.py
@@ -0,0 +1,14 @@
+import pyspark
+import json
+from pyspark.sql import SparkSession
+
+sc = pyspark.SparkContext('local[*]')
+spark = SparkSession.builder.getOrCreate()
+
+data = spark.read.json('../samples/sales.json', multiLine=True)
+# r1 = data.collect()
+
+counts = data.rdd.map(lambda row: (str(row["year"]) + "-" + str(row["month"]),row["value"])) \
+             .reduceByKey(lambda a, b: a +b)
+
+counts.saveAsTextFile("file:////srv/apps/its-bigdata-2020/balance-results")
