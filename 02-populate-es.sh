a8ade279a58097e9cb42d7991d600ef5676710f3
Steve Maraspin <s.maraspin@mvlabs.it>
2020-11-04 00:42:10 +0100

    ES index population script added

--- 02-populate-es.sh
+++ 02-populate-es.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 
+# We make sure no index named visualization already exists
+/usr/bin/curl -H "Content-Type: application/json" -XDELETE 'bigdata.its:9200/visualization'
+
+# We do use bulk insert to add contents of file samples/es-account-data.json into visualization index
+/usr/bin/curl -H "Content-Type: application/json" -XPOST 'bigdata.its:9200/visualization/_bulk?pretty&refresh' --data-binary "@samples/es-account-data.json"
