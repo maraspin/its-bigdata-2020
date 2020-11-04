c7a3456b6bc191b54ebcc0b46f408ef5b4096cee
Steve Maraspin <s.maraspin@mvlabs.it>
2020-11-04 01:05:00 +0100

    fixes on ES index creation/mapping/filling script

--- 02-populate-es.sh
+++ 02-populate-es.sh
@@ -6,8 +6,8 @@ INDEXNAME=visualize
 /usr/bin/curl -H "Content-Type: application/json" -XDELETE 'bigdata.its:9200/'$INDEXNAME
 
 # We do create a mapping for our new index visualize
-/usr/bin/curl -H "Content-Type: application/json" -XPUT '/'$INDEXNAME
-/usr/bin/curl -H "Content-Type: application/json" -XPUT '/'$INDEXNAME'/_mapping' --data-binary "@samples/es-account-mapping.json"
+/usr/bin/curl -XPUT 'bigdata.its:9200/'$INDEXNAME
+/usr/bin/curl -H "Content-Type: application/json" -XPUT 'bigdata.its:9200/'$INDEXNAME'/_mapping' --data-binary "@samples/es-account-mapping.json"
 
 # We do use bulk insert to add contents of file samples/es-account-data.json into visualization index
 /usr/bin/curl -H "Content-Type: application/json" -XPOST 'bigdata.its:9200/'$INDEXNAME'/_bulk?pretty&refresh' --data-binary "@samples/es-account-data.json"
